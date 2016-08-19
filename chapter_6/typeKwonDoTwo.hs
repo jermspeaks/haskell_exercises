c170ad30d9a77e282242244d11b0d37639aefd1d
jermspeaks <jermspeaks@gmail.com>
2016-08-19 12:55:21 +0700

    added disclaimer I didn't answer the question fully

--- chapter_6/typeKwonDoTwo.hs
+++ chapter_6/typeKwonDoTwo.hs
@@ -5,4 +5,4 @@ chk f x y = (f x) == y
 -- Problem 2
 arith :: Num b => (a -> b) -> Integer -> a -> b
 arith f i j = f j
-
+-- This is a partial answer. Still thinking through turning Integer into type 'b'
